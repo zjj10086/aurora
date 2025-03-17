52c3c1fe71605fd435096bee8e4659dd241a9139
张倔倔 <120997850+zjj10086@users.noreply.github.com>
2025-03-17 12:36:37 +0800

    Update expose.js

--- theme/aurora/expose.js
+++ theme/aurora/expose.js
@@ -1,15 +1,29 @@
 var ExposeConfig = {
   extraMenus: [
     {
-      groupTitle: '客服', // 大标题文本 （如需支持多语言，请在zh-CN.js、zh-TW.js、en-US.js中配置多语言文本）
+      groupTitle: 'Telegram', // 大标题文本 （如需支持多语言，请在zh-CN.js、zh-TW.js、en-US.js中配置多语言文本）
       groupLinks: [
         {
-          menuTitle: 'Telegram频道', // 标题文本
-          menuIcon: 'gauge', // 图标：https://phosphoricons.com/ 自选
-          menuPath: 'https://t.me/ELongCloud', // 链接地址
+          menuTitle: 'TG通知频道', // 标题文本
+          menuIcon: 'bell-ringing', // 图标：https://phosphoricons.com/ 自选
+          menuPath: 'https://t.me/xheiy', // 链接地址
           needSubscribe: false, // true：需要订阅后才能查看 false：无需订阅即可查看
           isExternal: true // true：打开新窗口 false：内嵌
         },
+        {
+          menuTitle: 'TG群组', // 标题文本
+          menuIcon: 'telegram-logo', // 图标：https://phosphoricons.com/ 自选
+          menuPath: 'https://t.me/lubiantany', // 链接地址
+          needSubscribe: true, // true：需要订阅后才能查看 false：无需订阅即可查看
+          isExternal: true // true：打开新窗口 false：内嵌
+        },
+        {
+          menuTitle: 'TG客服', // 标题文本
+          menuIcon: 'robot', // 图标：https://phosphoricons.com/ 自选
+          menuPath: 'https://t.me/yutuyy_bot', // 链接地址
+          needSubscribe: true, // true：需要订阅后才能查看 false：无需订阅即可查看
+          isExternal: true // true：打开新窗口 false：内嵌
+        }
       ]
     }
   ]
